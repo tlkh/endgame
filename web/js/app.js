ce71218a3f8db910d7efc670039acde74f5fe6fa
Timothy Liu <tlkh@live.co.uk>
2018-12-09 02:12:30 +0800

    minor fixes

--- web/js/app.js
+++ web/js/app.js
@@ -6,12 +6,13 @@ $(document).ready(function () {
     // table
     eel.expose(add_to_table);
 
-    sn = 0
+    var sn = 0
+
     function add_to_table(alert, tx, rx, packet) {
-        alert_add = "<small>"+alert+"</small>"
-        tx_add = "<small>"+tx+"</small>"
-        rx_add = "<small>"+rx+"</small>"
-        packet_add = "<span style='font-size:0.7em;width:400px;display:block;word-wrap:break-word;'>"+packet+"</span>"
+        var alert_add = "<small>" + alert + "</small>"
+        var tx_add = "<small>" + tx + "</small>"
+        var rx_add = "<small>" + rx + "</small>"
+        var packet_add = "<span style='font-size:0.7em;width:400px;display:block;word-wrap:break-word;'>" + packet + "</span>"
         console.log("Populating table...")
         devicetable.row.add([
             sn,
@@ -24,8 +25,8 @@ $(document).ready(function () {
     }
 
     eel.expose(update_capfiles);
-    
+
     function update_capfiles(number) {
-        capfiles_display.innerHTML = "<big><big>"+number+"</big></big>";
+        capfiles_display.innerHTML = "<big><big>" + number + "</big></big>";
     }
 });
